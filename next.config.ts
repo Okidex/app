c9de243161551cc165875ad5026c7b6feba6410b
Wilfred Wong <wilfredwong@Wilfreds-MacBook-Pro.local>
2025-10-21 23:13:22 +0800

    fix: Correct image domains in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -36,10 +36,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  webpack: (config) => {
-    config.resolve.alias['@'] = __dirname + '/src';
-    return config;
-  },
 };
 
 export default nextConfig;
