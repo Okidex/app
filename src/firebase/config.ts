7c237466ac01563097e51c32b390e17d5eac3887
Wilfred Wong <wilfredwong@okidex.com>
2026-04-23 17:11:43 +0530

    chore: force cache bust for API key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,3 +1,4 @@
+// Force cache bust to inject new API key
 export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
